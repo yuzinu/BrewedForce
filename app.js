893b2b4add3e3a6476b55d914f0558c527137a9d
Drew <drewwebs@gmail.com>
2020-10-05 15:43:48 -0400

    Populate entry file

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const User = require("./models/User");
+const Coffee = require("./models/Coffee");
 
 const users = require("./routes/api/users");
+const coffees = require("./routes/api/coffees");
+
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,6 +24,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
   
 app.use("/api/users", users);
-
+app.use("/api/coffees", coffees);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
--- routes/api/coffees.js
+++ routes/api/coffees.js
@@ -3,22 +3,13 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-const coffee = require('../../models/coffee');
-const validatecoffeeInput = require('../../validation/coffees');
+const Coffee = require('../../models/Coffee');
+const validateCoffeeInput = require('../../validation/coffees');
 
 router.get('/', (req, res) => {
     Coffee.find()
         .then(coffees => res.json(coffees))
         .catch(err => res.status(404).json({ nocoffeesfound: 'No coffees found' }));
-});
-
-router.get('/:coffee_id', (req, res) => {
-    Coffee.find({ coffee: req.params.coffee_id })
-        .then(coffees => res.json(coffees))
-        .catch(err =>
-            res.status(404).json({ nocoffeesfound: 'No coffees found with that name' }
-            )
-        );
 });
 
 router.get('/:id', (req, res) => {
