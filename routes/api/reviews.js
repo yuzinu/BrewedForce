78cf96d3aa47b58f49f8dc1044c6f317d943dadb
KUn Hee <kunhee617@gmail.com>
2020-10-05 23:48:38 -0400

    finished reviews routes

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -22,6 +22,24 @@ router.get('/user/:user_id', (req, res) => {
         );
 });
 
+router.get('/shop/:shop_id', (req, res) => {
+    Review.find({ shop: req.params.shop_id })
+        .then(reviews => res.json(reviews))
+        .catch(err =>
+            res.status(404).json({ noReviewsfound: 'No reviews found from that user' }
+            )
+        );
+});
+
+router.get('/coffee/:coffee_id', (req, res) => {
+    Review.find({ coffee: req.params.coffee_id })
+        .then(reviews => res.json(reviews))
+        .catch(err =>
+            res.status(404).json({ noReviewsfound: 'No reviews found from that user' }
+            )
+        );
+});
+
 router.get('/:id', (req, res) => {
     Review.findById(req.params.id)
         .then(review => res.json(review))
@@ -31,10 +49,46 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/',
+    passport.authenticate('jwt', { session: false}),
+    (req, res) => {
+
+        const { errors, isValid } = validateReviewInput(req.body);       
+        
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newReview = new Review({
+            // user: req.body.user.id,
+            // shop: req.body.shop.id,
+            // coffee: req.body.coffee.id,
+            // coffee_score: req.body.coffee_score.id,
+            rating: req.body.rating,
+            text: req.body.text
+        });
+
+        newReview
+            .save()
+            .then(review => res.json(review));
+    }
     
 );
 
-router.patch('/',
-    Review.findById(req.params.id)
+router.patch('/:id', passport.authenticate('jwt', { session: false }),
+        (req, res) => {
+        const { id } = req.params;
+        
+        const { errors, isValid } = validateReviewInput(req.body);      
+        
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
 
+  
+        Review.findByIdAndUpdate(id, req.body, { new: true }, (err, review) => {
+            res.json(review)})
+            .then(() => res.status(202).json('Review updated successfully.'))
+        }
 );
+
+module.exports = router;
