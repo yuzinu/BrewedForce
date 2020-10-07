0a41a77cb996d2336d136b7fc1e786225e2d7095
Eugene Moon <moonyoojin@gmail.com>
2020-10-07 15:18:30 -0400

    Complete google places search integration

--- frontend/src/actions/shop/shop_actions.js
+++ frontend/src/actions/shop/shop_actions.js
@@ -3,14 +3,19 @@ import * as GoogleApiUtil from '../../util/google_api_util';
 export const RECEIVE_NEARBY_SHOPS = 'RECEIVE_NEARBY_SHOPS';
 
 const receiveNearbyShops = shops => {
+  debugger
     return {
         type: RECEIVE_NEARBY_SHOPS,
         shops
     }
 };
 
-export const fetchNearbyShops = params => dispatch => {
-    return GoogleApiUtil.fetchNearbyShops(params).then(shops => (
-        dispatch(receiveNearbyShops(shops))
-    ))
-};
+export const fetchNearbyShops = params => {
+  return dispatch => {
+    return GoogleApiUtil.fetchNearbyShops(params)
+      .then(({data}) => {
+        debugger
+        dispatch(receiveNearbyShops(data))
+      })
+  }
+}
--- frontend/src/reducers/entities/shops_reducer.js
+++ frontend/src/reducers/entities/shops_reducer.js
@@ -3,11 +3,10 @@ import { RECEIVE_NEARBY_SHOPS } from '../../actions/shop/shop_actions';
 const shopsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
-
+    debugger;
     switch(action.type) {
         case RECEIVE_NEARBY_SHOPS:
-            nextState['nearbyShops'] = action.shops;
-            return nextState;
+            return action.shops;
         default:
             return state;
     }
