12ddc1db6ccf5a191e1bd93675eaca5ff278273a
Drew <drewwebs@gmail.com>
2020-10-07 11:25:53 -0400

    Default gray background for unstyled pages

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -34,7 +34,7 @@ class App extends React.Component {
       <> 
         <SideBarContainer />
         <Switch>
-          <AuthRoute exact path="/" component={MainPage} />
+          <Route exact path="/" component={MainPage} />
           <AuthRoute exact path="/login" component={LoginFormContainer} />
           <AuthRoute exact path="/signup" component={SignupFormContainer} />
           <ProtectedRoute exact path="/users/:userId" component={UserContainer} />
