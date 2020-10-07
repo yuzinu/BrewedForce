c46e616d5ab67e738d34f016dbbdef8a98268721
Drew <drewwebs@gmail.com>
2020-10-07 15:25:55 -0400

    Change things forever

--- frontend/src/components/coffee/coffee_index.jsx
+++ frontend/src/components/coffee/coffee_index.jsx
@@ -17,14 +17,24 @@ export default class CoffeeIndex extends React.Component {
         return this.state.coffees ? (
             <div className="coffee-index-container">
                 <ul>
-                {this.state.coffees.map(coffee => (
-                    <Link to={`/coffees/${coffee._id}`}>
-                        <li className="coffee-index-item" key={coffee._id}>
-                            <div className="coffee-index-item-name">{coffee.name}</div>
-                            <div className="coffee-index-item-origin">{coffee.origin}</div>
-                        </li>
-                    </Link>
-                ))}
+                {this.state.coffees.map((coffee, i) => {
+                    if (i < 3) {
+                        return <Link to={`/coffees/${coffee._id}`}>
+                            <li className="coffee-index-item-left" key={coffee._id}>
+                                <div className="coffee-index-item-name">{coffee.name}</div>
+                                <div className="coffee-index-item-origin">{coffee.origin}</div>
+                            </li>
+                        </Link>
+                    
+                    } else {
+                        return <Link to={`/coffees/${coffee._id}`}>
+                            <li className="coffee-index-item-right" key={coffee._id}>
+                                <div className="coffee-index-item-name">{coffee.name}</div>
+                                <div className="coffee-index-item-origin">{coffee.origin}</div>
+                            </li>
+                        </Link>
+                    }
+                })}
                 </ul>
             </div>
         ) : (
