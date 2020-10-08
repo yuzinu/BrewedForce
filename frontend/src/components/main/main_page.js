553da26555d892b4d76b492de47c144d24ed395c
Mark Camilleri <markcamilleri10@gmail.com>
2020-10-08 15:35:38 -0400

    Master changes

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 require('./main_page.scss');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 class MainPage extends React.Component {
 
   render() {
     return (
       <div className="main-page">
-        {/* <h1 className="coffee-header"></h1> */}
         <footer className="coffee-footer">
           Copyright &copy; 2020 BrewedForce by the Brewners
         </footer>
